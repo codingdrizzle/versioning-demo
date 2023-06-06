ceaf705d7cc17be75619193b52ed77df325aa30a
francis-buabin-owusu <francis.owusu@amalitech.com>
2023-06-06 01:40:22 +0000

    feat: added changelog generator

--- release.config.js
+++ release.config.js
@@ -1,10 +1,14 @@
 const config = {
     branches: ['main'],
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
+        ['@semantic-release/commit-analyzer', {
+            preset: 'conventional-changelog',
+        }],
+        ['@semantic-release/release-notes-generator', {
+            preset: 'conventional-changelog',
+        }],
         ["@semantic-release/git", {
-            "assets": ["dist/*.js", "dist/*.js.map"],
+            "assets": ["dist/*.js", "dist/*.js.map", "changelog.md"],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
