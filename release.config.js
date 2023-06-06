eec0510ef08134d8ba6d0beec26c945b43900c05
francis-buabin-owusu <francis.owusu@amalitech.com>
2023-06-06 01:46:57 +0000

    feat: removed all of the presets

--- release.config.js
+++ release.config.js
@@ -1,12 +1,8 @@
 const config = {
     branches: ['main'],
     plugins: [
-        ['@semantic-release/commit-analyzer', {
-            preset: 'conventional-changelog',
-        }],
-        ['@semantic-release/release-notes-generator', {
-            preset: 'conventional-changelog',
-        }],
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
         ["@semantic-release/git", {
             "assets": ["dist/*.js", "dist/*.js.map", "changelog.md"],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
