19c5d724cf1650b64e009f95dba759e821f28323
francis-buabin-owusu <francis.owusu@amalitech.com>
2023-06-12 09:47:12 +0000

    breaking_change: new api release

--- release.config.js
+++ release.config.js
@@ -1,7 +1,16 @@
 const config = {
     branches: ['main'],
     plugins: [
-        '@semantic-release/commit-analyzer',
+        ['@semantic-release/commit-analyzer', {
+            preset: 'conventionalcommits',
+            parserOpts: {
+                noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+            },
+            releaseRules: [
+                { type: 'BREAKING_CHANGE', release: 'major' },
+                { type: 'BREAKING_CHANGES', release: 'major' },
+            ],
+        }],
         '@semantic-release/release-notes-generator',
         ["@semantic-release/git", {
             "assets": ["dist/*.js", "dist/*.js.map", "changelog.md"],
