7a9e562e33b7994066c2ec9ffc98b6443b47f7ed
francis-buabin-owusu <francis.owusu@amalitech.com>
2023-06-14 08:15:59 +0000

    breaking_change: big change to this api

--- release.config.js
+++ release.config.js
@@ -13,7 +13,7 @@ const config = {
         }],
         '@semantic-release/release-notes-generator',
         ["@semantic-release/git", {
-            "assets": ["dist/*.js", "dist/*.js.map", "changelog.md"],
+            "assets": ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github' 
